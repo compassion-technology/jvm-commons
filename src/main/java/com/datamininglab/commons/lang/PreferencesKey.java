a3c0a7b05b3806701dba4f27abb0123868c1a5da
John Dimeo <dimeo@datamininglab.com>
2015-11-23 15:02:33 -0500

    Switch pref key to use primitive APIs

--- src/main/java/com/datamininglab/commons/lang/PreferencesKey.java
+++ src/main/java/com/datamininglab/commons/lang/PreferencesKey.java
@@ -4,7 +4,6 @@
  */
 package com.datamininglab.commons.lang;
 
-import java.util.Objects;
 import java.util.prefs.Preferences;
 
 import org.apache.commons.lang3.CharUtils;
@@ -23,36 +22,34 @@ public interface PreferencesKey {
 	default String key() { return toString().toLowerCase(); }
 	
 	default String get(Preferences node) {
-		return StringUtils.trimToNull(node.get(key(), null));
+		return StringUtils.defaultString(node.get(key(), null));
 	}
-	default Character getChar(Preferences node) {
-		return CharUtils.toCharacterObject(get(node));
+	default char getChar(Preferences node, char defVal) {
+		return CharUtils.toChar(get(node), defVal);
 	}
 	default boolean isOn(Preferences node) {
 		return node.getBoolean(key(), false);
 	}
-	default Long getLong(Preferences node) {
-		long ret = node.getLong(key(), Long.MIN_VALUE);
-		return ret == Long.MIN_VALUE? null : ret;
+	default long getLong(Preferences node, long defVal) {
+		return node.getLong(key(), defVal);
 	}
-	default Double getDouble(Preferences node) {
-		double ret = node.getDouble(key(), Double.NaN);
-		return Double.isNaN(ret)? null : ret; 
+	default double getDouble(Preferences node, double defVal) {
+		return node.getDouble(key(), defVal);
 	}
 	
 	default void put(Preferences node, String val) {
 		node.put(key(), StringUtils.defaultString(val));
 	}
-	default void put(Preferences node, Character val) {
-		put(node, Objects.toString(val, null));
+	default void put(Preferences node, char val) {
+		put(node, String.valueOf(val));
 	}
 	default void put(Preferences node, boolean flag) {
 		node.putBoolean(key(), flag);
 	}
-	default void put(Preferences node, Long val) {
-		node.putLong(key(), val == null? Long.MIN_VALUE : val.longValue());
+	default void put(Preferences node, long val) {
+		node.putLong(key(), val);
 	}
-	default void put(Preferences node, Double val) {
-		node.putDouble(key(), val == null? Double.NaN : val.doubleValue());
+	default void put(Preferences node, double val) {
+		node.putDouble(key(), val);
 	}
 }
