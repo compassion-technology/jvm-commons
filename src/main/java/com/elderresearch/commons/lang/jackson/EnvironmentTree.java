8b5bf2d5651387106ddc0f76fc403793a6441b4e
John Dimeo <dimeo@elderresearch.com>
2020-06-23 00:21:29 -0400

    Working environment override. Add a config util that loads the file and applies the override in one shot.

--- src/main/java/com/elderresearch/commons/lang/jackson/EnvironmentTree.java
+++ src/main/java/com/elderresearch/commons/lang/jackson/EnvironmentTree.java
@@ -6,33 +6,34 @@ import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.elderresearch.commons.lang.Utilities;
+import com.fasterxml.jackson.core.JsonPointer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.base.CaseFormat;
 
 import lombok.val;
 
 /**
- * This class creates a {@link JsonNode} tree from the current environment (including environment variables
- * via {@link System#getenv()} and system properties via {@link System#getProperties()}). This allows values
- * in the environment to override values in a configuration object.
+ * This class reads values starting with a a prefix from the current environment (including environment variables
+ * via {@link System#getenv()} and system properties via {@link System#getProperties()}) and applies them to an
+ * object via a {@link JsonNode} tree.
  * 
  * @author <a href="dimeo@elderresearch.com">John Dimeo</a>
  * @since Jun 13, 2020
  */
 public class EnvironmentTree {
 	private final String prefix;
-	private final ObjectNode root;
-	
 	private Map<String, String> environment;
 	
-	public EnvironmentTree(String prefix) {
-		this.prefix = prefix;
+	protected EnvironmentTree(String prefix) {
+		this.prefix = StringUtils.lowerCase(prefix);
 		this.environment = new HashMap<>();
-		this.root = JsonNodeFactory.instance.objectNode();
+	}
+	
+	public static EnvironmentTree forPrefix(String prefix) {
+		return new EnvironmentTree(prefix);
 	}
 
 	public EnvironmentTree addEnvironmentVariables() {
@@ -45,51 +46,37 @@ public class EnvironmentTree {
 		return this;
 	}
 	
-	public <T> T applyOverrides(ObjectMapper om, T obj) throws IOException {
-		JsonNode tree = om.valueToTree(obj);
-		
-		val trav = tree.traverse();
-		while (!trav.isClosed()) {
-			
-			
-			val fn = trav.nextFieldName();
-			if (fn != null) { System.out.println(trav.getParsingContext().pathAsPointer()); }
+	private void add(String path, String value) {
+		if (StringUtils.startsWithIgnoreCase(path, prefix)) {
+			val srcFmt = StringUtils.isAllUpperCase(StringUtils.replaceChars(path, "_- .", null))
+				? CaseFormat.UPPER_UNDERSCORE : CaseFormat.LOWER_CAMEL;
+			path = StringUtils.replaceChars(srcFmt.to(CaseFormat.LOWER_UNDERSCORE, path), "-.", "__");
+			environment.put(StringUtils.removeStart(path, prefix), value);
 		}
-		
-		return om.readerForUpdating(obj)
-			.without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-			.with(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES)
-			.readValue(root);
 	}
 	
-	private void add(String path, String value) {
-		path = StringUtils.removeStart(path, prefix);
-		
-		String[] pathArr;
-		if (StringUtils.contains(path, ".")) {
-			pathArr = StringUtils.split(path, ". ");
-		} else {
-			pathArr = StringUtils.split(path, "_- ");
-		}
-		
-		// Create intermediary objects
-		ObjectNode o = root;
-		for (int i = 0; i < pathArr.length - 1; i++) {
-			JsonNode child = o.get(pathArr[i]);
-			if (child == null) {
-				child = JsonNodeFactory.instance.objectNode();
-				o.set(pathArr[i], child);
-			}
-			try {
-				o = (ObjectNode) child;
-			} catch (ClassCastException e) {
-				System.err.println(path);
+	public <T> T applyOverrides(ObjectMapper om, T obj) throws IOException {
+		val tree = om.valueToTree(obj);
+		val trav = tree.traverse();
+		while (!trav.isClosed()) {
+			val fn = trav.nextFieldName();
+			if (fn == null) { continue; }
+			
+			val path = trav.getParsingContext().pathAsPointer();
+			val key = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE,
+					path.toString().replace(JsonPointer.SEPARATOR, '_'));
+			if (environment.containsKey(key)) {
+				ObjectNode n = Utilities.cast(tree.at(path));
+				n.put(last(path), environment.get(key));
 			}
 		}
 		
-		o.put(pathArr[pathArr.length - 1], value);
-		
-		// Also set the original/full path in case intermediate objects wasn't intended
-		root.put(path, value);
+		return om.readerFor(obj.getClass()).readValue(tree);
+	}
+	
+	private static String last(JsonPointer p) {
+		// p.last() is throwing an NPE :-/
+		int i = p.toString().lastIndexOf(JsonPointer.SEPARATOR);
+		return p.toString().substring(i + 1);
 	}
 }
