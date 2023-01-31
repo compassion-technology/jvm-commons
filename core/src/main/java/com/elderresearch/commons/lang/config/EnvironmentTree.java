14ee2b9e186b587260d14eb28949869607cef0f3
John Dimeo <jdimeo@us.ci.org>
2023-01-31 11:56:57 -0500

    Revert "Fixe to overriding configs using environments when using Jackson 2.14.  The underlying return type of tree.at() has changed to that ObjectNode can no longer be assumed."
    This reverts commit 8c1dfccdf8703a158c59f34764320e773fcca711.

--- core/src/main/java/com/elderresearch/commons/lang/config/EnvironmentTree.java
+++ core/src/main/java/com/elderresearch/commons/lang/config/EnvironmentTree.java
@@ -14,7 +14,6 @@ import com.fasterxml.jackson.core.JsonPointer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.node.TextNode;
 import com.google.common.base.CaseFormat;
 
 import lombok.val;
@@ -118,8 +117,8 @@ public class EnvironmentTree {
                 val key = prefix + CaseFormat.LOWER_CAMEL.to(env.pathFormat(),
                         path.toString().replace(JsonPointer.SEPARATOR, env.pathSeparator()));
                 if (env.has(key)) {
-                    ObjectNode n = Utilities.cast(tree.at(parent(path)));
-                    n.replace(last(path), TextNode.valueOf(env.get(key)));
+                    ObjectNode n = Utilities.cast(tree.at(path));
+                    n.put(last(path), env.get(key));
                 }
             }
             om.readerForUpdating(obj).readValue(tree);
@@ -138,8 +137,4 @@ public class EnvironmentTree {
 		val srcFmt = path.chars().anyMatch(Character::isLowerCase)? CaseFormat.LOWER_CAMEL : CaseFormat.UPPER_UNDERSCORE;
 		return StringUtils.replaceChars(srcFmt.to(CaseFormat.LOWER_UNDERSCORE, path), "-.", "__");
 	}
-	
-	private static String parent(JsonPointer p) {
-		int i = p.toString().lastIndexOf(JsonPointer.SEPARATOR);
-		return p.toString().substring(0,Math.max(i,1));	}
 }
