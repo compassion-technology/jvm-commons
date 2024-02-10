edb2e65e84646f8c65f833709381abc9c7c98e37
John Dimeo <jdimeo@us.ci.org>
2024-02-09 21:59:26 -0500

    Move YAML utils into commons

--- src/main/java/com/compassion/commons/jackson/YAMLUtils.java
+++ src/main/java/com/compassion/commons/jackson/YAMLUtils.java
@@ -1,11 +1,21 @@
 /* ©2017-2020 Elder Research, Inc. All rights reserved. */
 package com.compassion.commons.jackson;
 
+import java.util.function.Predicate;
+
+import org.apache.commons.lang3.StringUtils;
 import org.yaml.snakeyaml.DumperOptions.LineBreak;
+import org.yaml.snakeyaml.DumperOptions.ScalarStyle;
+import org.yaml.snakeyaml.nodes.MappingNode;
+import org.yaml.snakeyaml.nodes.Node;
+import org.yaml.snakeyaml.nodes.ScalarNode;
+import org.yaml.snakeyaml.nodes.Tag;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.fasterxml.jackson.dataformat.yaml.util.StringQuotingChecker;
 
+import lombok.val;
 import lombok.experimental.UtilityClass;
 
 /**
@@ -34,5 +44,60 @@ public class YAMLUtils {
 	 */
 	public YAMLMapper newMapper(LineBreak lineBreak) {
 		return new YAMLMapper(new YAMLFactoryCustom(lineBreak));
+	}
+	
+	/**
+	 * Look for a key in a YAML mapping node, returning {@code true} if the key is found and the callback returns
+	 * {@code true}.
+	 * @param <N> the type of node you expect to find at the key
+	 * @param n the root node
+	 * @param key the key
+	 * @param as the type of node you expect to find at the key
+	 * @param callback the callback to invoke with the value node if the key is found
+	 * @return the result of the callback if the node is found
+	 */
+	public static <N extends Node> boolean findInMap(MappingNode n, String key, Class<N> as, Predicate<N> callback) {
+		for (val tuple : n.getValue()) {
+			val keyNode = (ScalarNode) tuple.getKeyNode();
+			if (StringUtils.equalsIgnoreCase(key, keyNode.getValue())) {
+				return callback.test(as.cast(tuple.getValueNode()));
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Create a scalar node for the string, detecting the most appropriate {@link ScalarStyle} for the string.
+	 * @param s the string
+	 * @return a scalar node for the string
+	 * @see #detectScalarStyle(String)
+	 */
+	public static ScalarNode stringNode(String s) {
+		s = StringUtils.trimToNull(s);
+		return stringNode(s, detectScalarStyle(s));
+	}
+	
+	/**
+	 * Create a scalar node for the string, using the specified style.
+	 * @param s the string
+	 * @return a scalar node for the string
+	 */
+	public static ScalarNode stringNode(String s, ScalarStyle style) {
+		return new ScalarNode(Tag.STR, s, null, null, style);
+	}
+	
+	// This is copied/modified from YAMLGenerator.Feature.MINIMIZE_QUOTES implementation
+	private static final int MAX_ONE_LINE_LEN = 80;
+	public static ScalarStyle detectScalarStyle(String text) {
+		if (StringUtils.containsAny(text, "\r\n")) {
+            return ScalarStyle.LITERAL;
+		}
+		
+        // If one of reserved values ("true", "null"), or, number, preserve quoting:
+        if (text.length() > MAX_ONE_LINE_LEN || StringQuotingChecker.Default.instance().needToQuoteValue(text)) {
+        	return ScalarStyle.FOLDED;
+        }
+        
+        return ScalarStyle.PLAIN;
 	}
 }
