1eaf17a4a5d9130c82d593e07c8f6bf3f52e8c47
John Dimeo <jdimeo@us.ci.org>
2024-02-13 09:15:01 -0500

    Consolidate mixins in a single file

--- src/main/java/com/compassion/commons/jackson/ClientSecretMixin.java
+++
@@ -1,8 +0,0 @@
-package com.compassion.commons.jackson;
-
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
-public interface ClientSecretMixin {
-	@JsonSerialize(using = PasswordSerializer.class)
-	String getClientSecret();
-}
