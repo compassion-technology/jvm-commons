c57193679972ae3c48535c8986a3e350bf680065
John Dimeo <jdimeo@us.ci.org>
2024-06-20 23:14:07 -0400

    Move masking mixins to their config classes since they have to track closely with structure.
    Also, make the length of the **** match the original value which aids in debugging to see if param overrides worked.

--- core/src/main/java/com/compassion/commons/config/CredentialConfig.java
+++ core/src/main/java/com/compassion/commons/config/CredentialConfig.java
@@ -2,9 +2,9 @@ package com.compassion.commons.config;
 
 import java.util.function.Consumer;
 
-import com.compassion.commons.jackson.MaskingMixins.ApiKeyMixin;
-import com.compassion.commons.jackson.MaskingMixins.PasswordMixin;
+import com.compassion.commons.jackson.PasswordSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -22,6 +22,11 @@ public interface CredentialConfig {
 		public void forEachCredentialPath(Consumer<String> withSecretPath) {
 			withSecretPath.accept("apiKey");
 		}
+		
+		public interface Mixin {
+			@JsonSerialize(using = PasswordSerializer.class)
+			String getApiKey();
+		}
 	}
 	
 	@Getter @Setter @Accessors(chain = true)
@@ -33,6 +38,11 @@ public interface CredentialConfig {
 		public void forEachCredentialPath(Consumer<String> withSecretPath) {
 			withSecretPath.accept("user");
 			withSecretPath.accept("password");
+		}
+		
+		public interface Mixin {
+			@JsonSerialize(using = PasswordSerializer.class)
+			String getPassword();
 		}
 	}
 	
@@ -48,11 +58,38 @@ public interface CredentialConfig {
             withSecretPath.accept("privateKey");
             withSecretPath.accept("signature");
         }
+        
+        public interface Mixin {
+        	@JsonSerialize(using = PasswordSerializer.class)
+        	String getPrivateKey();
+        	@JsonSerialize(using = PasswordSerializer.class)
+        	String getSignature();
+        }
     }
 	
+	@Getter @Setter @Accessors(chain = true)
+	public class ConfigWithOAuth extends YAMLConfig implements CredentialConfig {
+		private String clientSecret;
+		private String clientId;
+		
+		@Override
+		public void forEachCredentialPath(Consumer<String> withSecretPath) {
+			withSecretPath.accept("clientSecret");
+			withSecretPath.accept("clientId");
+		}
+		
+		public interface Mixin {
+			@JsonSerialize(using = PasswordSerializer.class)
+			String getClientSecret();
+			@JsonSerialize(using = PasswordSerializer.class)
+			String getClientId();
+		}
+	}
+	
 	static void addMaskingMixins(ObjectMapper mapper) {
-		mapper
-			.addMixIn(ConfigWithApiKey.class, ApiKeyMixin.class)
-			.addMixIn(ConfigWithUserPassword.class, PasswordMixin.class);
+		mapper.addMixIn(ConfigWithApiKey.class,       ConfigWithApiKey.Mixin.class)
+		      .addMixIn(ConfigWithUserPassword.class, ConfigWithUserPassword.Mixin.class)
+		      .addMixIn(ConfigWithToken.class,        ConfigWithToken.Mixin.class)
+		      .addMixIn(ConfigWithOAuth.class,        ConfigWithOAuth.Mixin.class);
 	}
 }
--- core/src/main/java/com/compassion/commons/jackson/PasswordSerializer.java
+++ core/src/main/java/com/compassion/commons/jackson/PasswordSerializer.java
@@ -1,5 +1,18 @@
 package com.compassion.commons.jackson;
 
-public class PasswordSerializer extends ConstantSerializer {
-	public PasswordSerializer() { super(Object.class, "******"); }
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializerBase;
+
+public class PasswordSerializer extends ToStringSerializerBase {
+	private static final int MIN_LEN = 5;
+	
+	public PasswordSerializer() { super(Object.class); }
+
+	@Override
+	public String valueToString(Object value) {
+		return StringUtils.repeat("*", Math.max(Objects.toString(value).length(), MIN_LEN));
+	}
 }
