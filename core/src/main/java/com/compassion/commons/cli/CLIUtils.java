4e1fe021a41a89f497937b6ed780a85f51820cb9
John Dimeo <jdimeo@us.ci.org>
2024-08-08 09:34:20 -0400

    Add support for showing the full command tree in CLI help, not just direct children

--- core/src/main/java/com/compassion/commons/cli/CLIUtils.java
+++ core/src/main/java/com/compassion/commons/cli/CLIUtils.java
@@ -2,6 +2,7 @@ package com.compassion.commons.cli;
 
 import org.fusesource.jansi.AnsiConsole;
 
+import com.compassion.commons.Utilities;
 import com.compassion.commons.log.HasLoggingMixin;
 import com.compassion.commons.log.LoggingMixin;
 
@@ -9,9 +10,25 @@ import lombok.extern.log4j.Log4j2;
 import picocli.CommandLine;
 import picocli.CommandLine.ExitCode;
 import picocli.CommandLine.IExecutionExceptionHandler;
+import picocli.CommandLine.Model.UsageMessageSpec;
 
 @Log4j2
 public class CLIUtils {
+	public interface CLIOptions {
+		/**
+		 * Instead of showing just the "direct child" subcommands of the root, show the full
+		 * tree of all subcommands in the CLI's help. 
+		 */
+		int HELP_COMMAND_TREE = 1 << 0;
+		
+		/**
+		 * When parsing enum options, allow any casing to match.
+		 */
+		int ENUMS_CASE_INSENSITIVE = 1 << 1;
+		
+		int ALL = HELP_COMMAND_TREE | ENUMS_CASE_INSENSITIVE;
+	}
+	
 	/**
 	 * Sets common system properties needed in CLIs, like expanding the width of the CLI
 	 * help to 100 characters (which fits on most modern laptops/monitors) and disabling
@@ -23,21 +40,33 @@ public class CLIUtils {
 		System.setProperty("picocli.usage.width", "100");
 	}
 	
+	/**
+	 * See {@link #execRoot(Object, int, String...)}.
+	 * This method specifies all options by default. 
+	 * @param cli the CLI implementation
+	 * @param args the command line args
+	 */
+	public static void execRoot(Object cli, String... args) {
+		execRoot(cli, CLIOptions.ALL, args);
+	}
 	
 	/**
 	 * Initializes the CLI and executes it, exiting the JVM with the returned exit code. Includes:
 	 * <ul><li>Installing and uninstalling an {@link AnsiConsole}</li>
-	 * <li>Setting common system properties via {@link #initSysProps()}<li>
-	 * <li>Everything in {@link #exec(Object, String...)}</ul>
+	 * <li>Setting common system properties via {@link #initSysProps()}</li>
+	 * <li>Everything in {@link #exec(Object, String...)}</li></ul>
 	 * @param cli the CLI implementation
+	 * @param options bitwise options that customize the CLI's behavior. For example
+	 * {@code CLIOptions.HELP_COMMAND_TREE | CLIOptions.ENUMS_CASE_INSENSITIVE}
 	 * @param args the command line args
 	 * @see ExitCode
 	 * @see System#exit(int)
+	 * @see CLIOptions
 	 */
-	public static void execRoot(Object cli, String... args) {
+	public static void execRoot(Object cli, int options, String... args) {
 		initSysProps();
 		AnsiConsole.systemInstall();
-		var ret = exec(cli, args);
+		var ret = exec(cli, options, args);
 		AnsiConsole.systemUninstall();
 		System.exit(ret);
 	}
@@ -50,11 +79,14 @@ public class CLIUtils {
 	 * <li>If the CLI uses the {@link LoggingMixin} to dynamically adjust the log level,
 	 * register the logging mixin execution strategy</li></ul>
 	 * @param cli the CLI implementation
+	 * @param options bitwise options that customize the CLI's behavior. For example
+	 * {@code CLIOptions.HELP_COMMAND_TREE | CLIOptions.ENUMS_CASE_INSENSITIVE}
 	 * @param args the command line args
 	 * @return the exit code
 	 * @see ExitCode
+	 * @see CLIOptions
 	 */
-	public static int exec(Object cli, String... args) {
+	public static int exec(Object cli, int options, String... args) {
 		// Reroute any exceptions to the default handler, which delegates to COFTool's handling.
 		// This unifies exception handling across CLIs and other contexts, like Lambda
 		IExecutionExceptionHandler exHandler = (e, cmd, res) -> {
@@ -69,9 +101,12 @@ public class CLIUtils {
 		
 		var cmd = new CommandLine(cli)
 			.setExecutionExceptionHandler(exHandler)
-			.setCaseInsensitiveEnumValuesAllowed(true);
+			.setCaseInsensitiveEnumValuesAllowed(Utilities.isOn(options, CLIOptions.ENUMS_CASE_INSENSITIVE));
 		if (cli instanceof HasLoggingMixin) {
 			cmd.setExecutionStrategy(LoggingMixin::executionStrategy);
+		}
+		if (Utilities.isOn(options, CLIOptions.HELP_COMMAND_TREE)) {
+	        cmd.getHelpSectionMap().put(UsageMessageSpec.SECTION_KEY_COMMAND_LIST, new CommandTreeHelp());
 		}
 		return cmd.execute(args);
 	}
