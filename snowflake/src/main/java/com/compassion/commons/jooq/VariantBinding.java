836e4eececc0ba147b8f392146a80a21542328c2
John Dimeo <jdimeo@us.ci.org>
2024-10-01 17:21:17 -0400

    Add a prepared statement binding, but I don't think this will work to bind a function

--- snowflake/src/main/java/com/compassion/commons/jooq/VariantBinding.java
+++ snowflake/src/main/java/com/compassion/commons/jooq/VariantBinding.java
@@ -3,12 +3,18 @@ package com.compassion.commons.jooq;
 import java.sql.SQLException;
 
 import org.jooq.BindingSQLContext;
+import org.jooq.BindingSetStatementContext;
 import org.jooq.impl.DefaultBinding;
 import org.jooq.impl.SQLDataType;
 
 public class VariantBinding extends DefaultBinding<byte[], byte[]> {
 	public VariantBinding() {
 		super(DefaultBinding.binding(SQLDataType.VARBINARY));
+	}
+	
+	@Override
+	public void set(BindingSetStatementContext<byte[]> ctx) throws SQLException {
+		ctx.statement().setString(ctx.index(), "parse_json($$" + new String(ctx.value()) + "$$)");
 	}
 
 	@Override
