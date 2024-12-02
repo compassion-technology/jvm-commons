df0c598dcc00852b9ebcdc5edcaa1265a7f13361
John Dimeo <jdimeo@us.ci.org>
2024-12-02 17:02:01 -0500

    Add PIcocli converter

--- core/src/main/java/com/compassion/commons/cli/CLIStringSet.java
+++ core/src/main/java/com/compassion/commons/cli/CLIStringSet.java
@@ -7,10 +7,13 @@ import org.apache.commons.lang3.StringUtils;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 import lombok.NoArgsConstructor;
+import picocli.CommandLine.ITypeConverter;
+import picocli.CommandLine.Parameters;
 
 /**
  * A simple string set implementation for CLI and YAML config options (since it supports parsing
- * a CSV or semicolon separated environment variable override).
+ * a CSV or semicolon separated environment variable override). Use {@link CLIStringSetConverter}
+ * with a {@link Parameters} annotation.
  */
 @NoArgsConstructor
 public class CLIStringSet extends LinkedHashSet<String> {
@@ -27,5 +30,12 @@ public class CLIStringSet extends LinkedHashSet<String> {
 		// Provide two options so you can use semicolons if you have commas in your values
 		if (s.contains(";")) { return new CLIStringSet(StringUtils.split(s, ';')); }
 		return new CLIStringSet(StringUtils.split(s, ','));
+	}
+	
+	public static class CLIStringSetConverter implements ITypeConverter<CLIStringSet> {
+		@Override
+		public CLIStringSet convert(String value) throws Exception {
+			return CLIStringSet.parse(value);
+		}
 	}
 }
