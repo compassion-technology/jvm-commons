d10de26c27c95d9b8c7b3368a286d0f40a696182
John Dimeo <jdimeo@us.ci.org>
2024-12-16 10:26:54 -0500

    Change class name to disambiguate with Pulsar

--- core/src/main/java/com/compassion/commons/aws/LambdaContextEnvironment.java
+++ core/src/main/java/com/compassion/commons/aws/LambdaContextEnvironment.java
@@ -7,8 +7,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.compassion.commons.config.ConfigEnvironment.ConfigEnvironmentMap;
 import com.google.common.base.CaseFormat;
 
-public class ContextEnvironment extends ConfigEnvironmentMap {
-	public ContextEnvironment(Context c) {
+public class LambdaContextEnvironment extends ConfigEnvironmentMap {
+	public LambdaContextEnvironment(Context c) {
 		super('_', CaseFormat.LOWER_UNDERSCORE);
 		Optional.ofNullable(c.getClientContext()).map(ClientContext::getCustom).ifPresent(this::putAll);
 	}
