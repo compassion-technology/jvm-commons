b0cc14d0258f543f55825f693693066c45a9eb49
Riley Graudin <rgraudin@us.ci.org>
2025-02-27 16:19:45 -0500

    Promote to main and mapper inclusion

---
+++ metrics/src/main/java/com/compassion/commons/metrics/DataDogTags.java
@@ -0,0 +1,27 @@
+package com.compassion.commons.metrics;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+public interface DataDogTags {
+	static ObjectMapper mapper = new YAMLMapper()
+		.configure(YAMLGenerator.Feature.USE_PLATFORM_LINE_BREAKS, false)
+		.configure(YAMLGenerator.Feature.WRITE_DOC_START_MARKER, false)
+		.configure(YAMLGenerator.Feature.MINIMIZE_QUOTES, true)
+		.setSerializationInclusion(Include.NON_EMPTY);
+	
+	default List<String> tagList() {
+		try {
+			return List.of(StringUtils.split(mapper.writeValueAsString(this).replace(": ", ":"), '\n'));
+		} catch (JsonProcessingException e) {
+			throw new IllegalStateException("Cannot serialize DataDog tags", e);
+		}
+	}
+}
