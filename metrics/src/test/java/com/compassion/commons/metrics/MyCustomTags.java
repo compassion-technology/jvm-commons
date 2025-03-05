3162e3a9e851783a47a741e6840f782f4ee2a1f1
Riley Graudin <rgraudin@us.ci.org>
2025-03-05 14:41:03 -0500

    Basic approach to getting annotation metadata with current approach.

--- metrics/src/test/java/com/compassion/commons/metrics/MyCustomTags.java
+++ metrics/src/test/java/com/compassion/commons/metrics/MyCustomTags.java
@@ -1,21 +1,29 @@
 package com.compassion.commons.metrics;
 
+import com.compassion.commons.LambdaUtils;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Setter @Getter
 @JsonPropertyOrder(alphabetic = true)
-@JsonNaming(SnakeCaseStrategy.class)
+@JsonNaming(SnakeCaseStrategy.class) @NoArgsConstructor
 public class MyCustomTags {
 	private String environment;
 	private boolean haveZildjians;
 	private String version;
+	
+	
+	public MyCustomTags(MyStandardClass base) {
+		LambdaUtils.accept(base.getName(),       this::setEnvironment);
+		LambdaUtils.accept(base.getCofVersion(), this::setVersion);
+	}
 	
 	@RequiredArgsConstructor(staticName = "wrap")
 	public static class MyCustomTagsContainer implements DataDogTags {
