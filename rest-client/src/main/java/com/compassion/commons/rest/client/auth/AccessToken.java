7a831f116cf912a4a5114d94e66135683ee8f6e8
John Dimeo <jdimeo@us.ci.org>
2025-04-05 07:23:28 -0400

    Each OAuth method requires a different request body (empty JSON vs. empty form). Re-use existing clients to create session tokens.
    # Conflicts:
    #	rest-client/src/main/java/com/compassion/commons/rest/client/auth/AccessToken.java

--- rest-client/src/main/java/com/compassion/commons/rest/client/auth/AccessToken.java
+++ rest-client/src/main/java/com/compassion/commons/rest/client/auth/AccessToken.java
@@ -6,7 +6,6 @@ import com.compassion.commons.rest.client.RecursiveTarget;
 import com.compassion.commons.rest.client.RestClient;
 import com.compassion.commons.rest.client.WebParam;
 
-import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.Form;
 import jakarta.ws.rs.core.Response.Status.Family;
@@ -16,19 +15,17 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public abstract class AccessToken {
-	public static IAccessToken acquire(OAuthMethod method, OAuthAwareConfig config) throws IOException {
+	public static IAccessToken acquire(RestClient client, OAuthMethod method, OAuthAwareConfig config) throws IOException {
 		log.info("Acquiring new access token for {}", config.baseUrl());
 		
-		try (var client = new RestClient(ClientBuilder.newBuilder())) {
-			var ret = client.request(RecursiveTarget.newTarget(config.baseUrl()), config.authCreds())
-				.post(method.getRequestBody());
-			
-			if (ret.getStatusInfo().getFamily() != Family.SUCCESSFUL) {
-				throw new IOException(
-					String.format("Retrieving access token failed with status code %d and details: %s", ret.getStatus(), ret.readEntity(String.class)));
-			}
-			return ret.readEntity(method.getPojo());
+		var ret = client.request(RecursiveTarget.newTarget(config.baseUrl()), config.authCreds())
+			.post(method.getRequestBody());
+		
+		if (ret.getStatusInfo().getFamily() != Family.SUCCESSFUL) {
+			throw new IOException(
+				String.format("Retrieving access token failed with status code %d and details: %s", ret.getStatus(), ret.readEntity(String.class)));
 		}
+		return ret.readEntity(method.getPojo());
 	}
 	
 	public interface IAccessToken {
