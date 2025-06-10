8ea3dc7f6d03bd14174f342d53cc3bf32178dee3
John Dimeo <jdimeo@us.ci.org>
2025-06-09 22:43:51 -0400

    Add Dropwizard config factory that bridges config frameworks and applies env overrides to the Dropwizard loaded config object

---
+++ rest-server/src/main/java/com/compassion/commons/rest/server/ConfigFactory.java
@@ -0,0 +1,58 @@
+package com.compassion.commons.rest.server;
+
+import java.io.IOException;
+import java.util.function.UnaryOperator;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import com.compassion.commons.config.Config;
+import com.compassion.commons.config.ConfigOverrides;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.dropwizard.configuration.ConfigurationException;
+import io.dropwizard.configuration.ConfigurationFactory;
+import io.dropwizard.configuration.ConfigurationSourceProvider;
+import io.dropwizard.configuration.DefaultConfigurationFactoryFactory;
+import io.dropwizard.configuration.YamlConfigurationFactory;
+import jakarta.validation.Validator;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * Bridge Compassion commons config framework and Dropwizard config.
+ * @param <C> the configuration implementation class
+ */
+@Log4j2
+@Accessors(chain = true)
+public class ConfigFactory<C extends Config> extends YamlConfigurationFactory<C> {
+	private String prefix;
+	
+	@Setter
+	private ObjectMapper loggingMapper = new ObjectMapper();
+	@Setter
+	private UnaryOperator<ConfigOverrides> overrides = $ -> $.withEnvironmentVariables().withSystemProperties();
+	
+	public ConfigFactory(Class<C> klass, @Nullable Validator validator, ObjectMapper objectMapper,
+			String propertyPrefix) {
+		super(klass, validator, objectMapper, propertyPrefix);
+		this.prefix = propertyPrefix;
+	}
+
+	@Override
+	public C build(ConfigurationSourceProvider provider, String path) throws IOException, ConfigurationException {
+		var ret = super.build(provider, path);
+		ret = Config.load(log, mapper, ret, overrides.apply(ConfigOverrides.forPrefixes(prefix)));
+		if (loggingMapper != null) {
+			ret.logConfig(loggingMapper);
+		}
+		return ret;
+	}
+	
+	public static class ConfigFactoryFactory<T extends Config> extends DefaultConfigurationFactoryFactory<T> {
+		@Override
+		public ConfigurationFactory<T> create(Class<T> klass, Validator validator, ObjectMapper mapper, String prefix) {
+			return new ConfigFactory<>(klass, validator, configureObjectMapper(mapper), prefix);
+		}
+	}
+}
