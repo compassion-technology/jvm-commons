c84594cdb81c22bcb37bb2b1296fcb0b9bd1bdc0
John Dimeo <jdimeo@us.ci.org>
2025-06-09 22:59:12 -0400

    Register modules on logging mapper, support a prefix override since it's hardcoded to "dw" for Dropdwizard

--- rest-server/src/main/java/com/compassion/commons/rest/server/ConfigFactory.java
+++ rest-server/src/main/java/com/compassion/commons/rest/server/ConfigFactory.java
@@ -3,11 +3,13 @@ package com.compassion.commons.rest.server;
 import java.io.IOException;
 import java.util.function.UnaryOperator;
 
+import org.apache.commons.lang3.StringUtils;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import com.compassion.commons.config.Config;
 import com.compassion.commons.config.ConfigOverrides;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import io.dropwizard.configuration.ConfigurationException;
 import io.dropwizard.configuration.ConfigurationFactory;
@@ -15,6 +17,7 @@ import io.dropwizard.configuration.ConfigurationSourceProvider;
 import io.dropwizard.configuration.DefaultConfigurationFactoryFactory;
 import io.dropwizard.configuration.YamlConfigurationFactory;
 import jakarta.validation.Validator;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import lombok.extern.log4j.Log4j2;
@@ -26,10 +29,10 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @Accessors(chain = true)
 public class ConfigFactory<C extends Config> extends YamlConfigurationFactory<C> {
-	private String prefix;
+	private final String prefix;
 	
 	@Setter
-	private ObjectMapper loggingMapper = new ObjectMapper();
+	private ObjectMapper loggingMapper = new YAMLMapper().findAndRegisterModules();
 	@Setter
 	private UnaryOperator<ConfigOverrides> overrides = $ -> $.withEnvironmentVariables().withSystemProperties();
 	
@@ -49,10 +52,13 @@ public class ConfigFactory<C extends Config> extends YamlConfigurationFactory<C>
 		return ret;
 	}
 	
+	@RequiredArgsConstructor
 	public static class ConfigFactoryFactory<T extends Config> extends DefaultConfigurationFactoryFactory<T> {
+		private final String prefix;
+		
 		@Override
-		public ConfigurationFactory<T> create(Class<T> klass, Validator validator, ObjectMapper mapper, String prefix) {
-			return new ConfigFactory<>(klass, validator, configureObjectMapper(mapper), prefix);
+		public ConfigurationFactory<T> create(Class<T> klass, Validator validator, ObjectMapper mapper, String dwPrefix) {
+			return new ConfigFactory<>(klass, validator, configureObjectMapper(mapper), StringUtils.defaultIfBlank(prefix, dwPrefix));
 		}
 	}
 }
