ebde5307ec731d41f33616c792abe50b6c03ff32
John Dimeo <jdimeo@us.ci.org>
2025-08-07 22:49:38 -0400

    Working version that also supports filtering by the Environment tag and reloading keys after an interval

--- rest-server/src/main/java/com/compassion/commons/rest/server/aws/ApiGatewayKeyAuthenticator.java
+++ rest-server/src/main/java/com/compassion/commons/rest/server/aws/ApiGatewayKeyAuthenticator.java
@@ -1,12 +1,18 @@
 package com.compassion.commons.rest.server.aws;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import com.compassion.commons.Utilities;
+import com.compassion.commons.config.CIEnvironment;
+
 import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.Authenticator;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient;
 
@@ -15,21 +21,41 @@ import software.amazon.awssdk.services.apigateway.ApiGatewayClient;
 public class ApiGatewayKeyAuthenticator implements Authenticator<String, ApiGatewayPrincipal> {
 	private final String apiId;
 	private final Map<String, ApiGatewayPrincipal> map = new HashMap<>();
+
+	private Instant lastReloaded;
+	
+	@Setter
+	private Duration reloadInterval = Duration.ofHours(4);
+	@Setter
+	private CIEnvironment environment;
 	
 	public void reload() {
 		map.clear();
 		
 		var client = ApiGatewayClient.create();
 		client.getApiKeysPaginator(req -> req.includeValues(true)).forEach(resp -> resp.items().forEach(key -> {
-			if (apiId.equals(key.tags().get("API"))) {
-				map.put(key.value(), ApiGatewayPrincipal.forKey(key));
+			if (!apiId.equals(key.tags().get("API"))) {
+				log.debug("Skipping API key {} {}, wrong API", key.id(), key.name());
+				return;
 			}
+			
+			if (environment != null && CIEnvironment.valueOf(key.tags().get("Environment")) != environment) {
+				log.debug("Skipping API key {} {}, wrong environment", key.id(), key.name());
+				return;
+			} 
+			
+			map.put(key.value(), ApiGatewayPrincipal.forKey(key));
 		}));
-		log.info("Loaded {} API keys from AWS", map.size());
+		log.info("Loaded {} API keys from AWS for API {}", map.size(), apiId);
+		lastReloaded = Instant.now();
 	}
 	
 	@Override
 	public Optional<ApiGatewayPrincipal> authenticate(String credentials) throws AuthenticationException {
+		if (lastReloaded == null || Duration.between(lastReloaded, Instant.now()).compareTo(reloadInterval) > 0) {
+			reload();
+		}
+		
 		return Optional.ofNullable(map.get(credentials));
 	}
 }
--- rest-server/src/main/java/com/compassion/commons/rest/server/aws/ApiKeyAuthFilter.java
+++ rest-server/src/main/java/com/compassion/commons/rest/server/aws/ApiKeyAuthFilter.java
@@ -2,14 +2,21 @@ package com.compassion.commons.rest.server.aws;
 
 import java.io.IOException;
 
+import org.apache.commons.lang3.StringUtils;
+
 import io.dropwizard.auth.AuthFilter;
 import io.dropwizard.auth.Authenticator;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.core.SecurityContext;
 
 public class ApiKeyAuthFilter extends AuthFilter<String, ApiGatewayPrincipal> {
-	private static final String API_KEY_HEADER = "x-api-key";
+	static final String API_KEY_HEADER = "x-api-key";
+	static final String API_KEY_PREFIX = "ApiKey"; 
 
+	public ApiKeyAuthFilter() {
+		prefix = API_KEY_PREFIX;
+	}
+	
 	public ApiKeyAuthFilter setAuthenticator(Authenticator<String, ApiGatewayPrincipal> a) {
 		authenticator = a;
 		return this;
@@ -17,8 +24,8 @@ public class ApiKeyAuthFilter extends AuthFilter<String, ApiGatewayPrincipal> {
 	
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
-        var credentials = requestContext.getHeaders().getFirst(API_KEY_HEADER);
-        if (!authenticate(requestContext, credentials, SecurityContext.BASIC_AUTH)) {
+        var credentials = StringUtils.substringAfter(requestContext.getHeaders().getFirst(API_KEY_HEADER), API_KEY_PREFIX);
+        if (!authenticate(requestContext, StringUtils.stripToNull(credentials), SecurityContext.DIGEST_AUTH)) {
             throw unauthorizedHandler.buildException(prefix, realm);
 	    }
 	}
